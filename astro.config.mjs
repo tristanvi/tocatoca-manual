2f1775b666c0a0d4e1c2e18f3e1ec02ce85fdd07
Christian Xell <donxello@tangotunes.com>
2026-05-31 08:55:42 +0200

    Add production site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+  site: 'https://manual.tocatoca.com',
   integrations: [
     starlight({
       title: 'TocaToca Handbuch',
